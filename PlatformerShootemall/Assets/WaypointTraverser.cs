492baaf24d24d7111e7823d08a1a315a981c5440
Arvid-00 <62692818+Arvid-00@users.noreply.github.com>
2023-04-19 23:00:51 +0200

    Added/Updated terrain

---
+++ PlatformerShootemall/Assets/WaypointTraverser.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointTraverser : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    private int currentTarget = 0;
+
+    public bool reverse; //if true, it reverse the platform backwards. Otherwise it loops from the start
+    public int direction = 1;
+
+    public float speed = 2f;
+
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentTarget].transform.position, transform.position) < 0.1f)
+        {
+            if (!reverse)
+            {
+                currentTarget++;
+                if (currentTarget >= waypoints.Length)
+                {
+                    currentTarget = 0;
+                }
+            }
+            else
+            {
+                currentTarget = currentTarget + direction;
+                if (currentTarget >= waypoints.Length - 1)
+                    direction = -1;
+                if (currentTarget <= 0)
+                    direction = 1;
+            }
+
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentTarget].transform.position, Time.deltaTime * speed);
+    }
+}
