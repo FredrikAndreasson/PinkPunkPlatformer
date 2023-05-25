2a8bec60a648e2244488178f2d8927ff6b87c476
FredrikAndreasson <fredrik.andreasson@yandex.com>
2023-05-25 16:01:04 +0200

    Chain of responsibility base

--- PlatformerShootemall/Assets/Scripts/Fire.cs
+++ PlatformerShootemall/Assets/Scripts/Fire.cs
@@ -1,23 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Fire : MonoBehaviour
 {
-    float timer;
-    public float activationTimer;
     public Sprite off;
-    void Update()
-    {
-        timer += Time.deltaTime;
-        if(timer > activationTimer)
-        {
-            this.gameObject.GetComponent<Animator>().enabled = false;
-            this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = off;
 
-            //There are 2 collders, ON collision with player, you can seperate by checking "on trigger" or not
-        }
+    public void TurnOff()
+    {
+        this.gameObject.GetComponent<Animator>().enabled = false;
+        this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = off;
     }
 
 }
---
+++ PlatformerShootemall/Assets/Scripts/WinConditions.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WinConditions : MonoBehaviour
+{
+    private int totalFruit;
+    private int fruitLimit;
+    [SerializeField] private TextMeshPro text;
+
+    [SerializeField] private Fire[] fires;
+
+    private void Start()
+    {
+        totalFruit = GameObject.FindGameObjectsWithTag("Fruit").Length;
+        fruitLimit = totalFruit / 2;
+        text.text = "You need a total of: " + fruitLimit + " fruit to pass";
+    }
+
+    void Update()
+    {
+        //Authenticate
+        if (GameObject.FindGameObjectsWithTag("Fruit").Length < fruitLimit)
+        {
+            return;
+        }
+
+        //Log
+        Log(totalFruit, GameManager.instance.score);
+
+        //Authorize
+        Authorize();
+    }
+
+    private void Log(int totalFruits, int collected)
+    {
+        text.text = "You passed the level!!! You have " + collected + "/" + totalFruits + " fruits";
+    }
+
+    private void Authorize()
+    {
+        for (int i = 0; i < fires.Length; i++)
+        {
+            fires[i].TurnOff();
+        }
+    }
+}
